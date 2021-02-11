a99de05cd34d8d234cd7769984d2c4a95d29c107
Denis <cinets0@gmail.com>
2021-02-11 18:31:14 +0400

    Исправления

--- js/main.js
+++ js/main.js
@@ -17,48 +17,65 @@ const getRandomOneArray = (arr) => {
   return arr[Math.floor(Math.random() * arr.length)]
 }
 
-const typeOfBuildings = ['palace', 'flat', 'house', 'bungalow'];
-const features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const checkTime = ['12:00', '13:00', '14:00'];
+const TYPE_OF_BUILDINGS = ['palace', 'flat', 'house', 'bungalow'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const CHECK_TIME = ['12:00', '13:00', '14:00'];
 
 const getRandomArray = (arr) => {
   return arr.slice(0, getRandomNumber(0, arr.length))
 };
 
-const PRICE = getRandomNumber(1, 100000);
-const ROOMS = getRandomNumber(1, 4);
-const GUESTS = getRandomNumber(1, 8);
+const PRICE = {
+  min: 8000,
+  max: 40000,
+};
+const ROOMS = {
+  min: 1,
+  max: 10,
+};
+const GUESTS = {
+  min: 1,
+  max: 10,
+}
+
 const LOCATION_X_MIN = 35.65000;
 const LOCATION_X_MAX = 35.70000;
 const LOCATION_Y_MIN = 139.70000;
 const LOCATION_Y_MAX = 139.80000;
 const FLOATING_POINT_NUMBER = 5;
 
-const getArray = function (price, rooms, guests) {
-  return [
-    {
-      objName: 'author',
+const getObj = function () {
+  let locationX = getFloatingRandomNumber(LOCATION_X_MIN, LOCATION_X_MAX, FLOATING_POINT_NUMBER);
+  let locationY = getFloatingRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX, FLOATING_POINT_NUMBER);
+  return {
+    author: {
       avatar: `img/avatars/user0${getRandomNumber(1, 8)}.png`,
     },
-    {
-      objName: 'offer',
+    offer: {
       title: 'Заголовок придуманный самостоятельно',
-      address: [location.x, location.y],
-      price: price,
-      type: getRandomOneArray(typeOfBuildings),
-      rooms: rooms,
-      guests: guests,
-      checkin: getRandomOneArray(checkTime),
-      checkout: getRandomOneArray(checkTime),
-      features: getRandomArray(features),
+      address: [locationX, locationY],
+      price: getRandomNumber(PRICE.min, PRICE.max),
+      type: getRandomOneArray(TYPE_OF_BUILDINGS),
+      rooms: getRandomNumber(ROOMS.min, ROOMS.max),
+      guests: getRandomNumber(GUESTS.min, GUESTS.max),
+      checkin: getRandomOneArray(CHECK_TIME),
+      checkout: getRandomOneArray(CHECK_TIME),
+      features: getRandomArray(FEATURES),
       description: 'Самостоятельно придуманное описание помещения',
       photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'],
-      location: {
-        x: getFloatingRandomNumber(LOCATION_X_MIN, LOCATION_X_MAX, FLOATING_POINT_NUMBER),
-        y: getFloatingRandomNumber(LOCATION_Y_MIN, LOCATION_Y_MAX, FLOATING_POINT_NUMBER),
-      },
     },
-  ]
+    location: {
+      x: locationX,
+      y: locationY,
+    },
+  };
 };
 
-getArray(PRICE, ROOMS, GUESTS);
+const array = [];
+const getArray = () => {
+  array.push(getObj());
+  return array;
+};
+for (let i = 0; i < 10; i++) {
+  getArray();
+}
